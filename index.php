9b8c45fd7d73093baf842cb146ac514844b362b6
GustavoFeitosa2511 <gustavobruxo6@gmail.com>
2025-05-09 20:18:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+        <title> Dr.tec </title>
+        <link rel="styleshect" type="text/css" href="style.css" media="screen"/>
+    </head>
+    <body>
+        <header>      
+        </header> <main>
+        <footer>    
+        </footer> </main>
+
+    </body>
+</html>
